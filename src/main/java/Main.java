b0a205a118f071f80339203c7e5c87357b675a96
cyl1 <120377796@umail.ucc.ie>
2022-11-26 16:03:09 +0000

    general changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,7 @@
-package org.ucc.a03;
-
 public class Main {
     public static void main(String[] args) {
 
-        var rgb = 16777215; // hex = 0F1419, r = 15, g = 20, g = 25
+        //var rgb = 16777215; // hex = 0F1419, r = 15, g = 20, g = 25
 
         Colour c1 = new Colour(0.5f,1.0f,0.2f);
         Colour c2 = new Colour(6915853);
