7c855a072551e60e75040b0bf48bb058e63df006
cyl1 <120377796@umail.ucc.ie>
2022-11-27 15:13:09 +0000

    made test names descriptive

--- src/test/java/ColourTest.java
+++ src/test/java/ColourTest.java
@@ -8,22 +8,23 @@ class ColourTest {
     private final static Colour colourInt = new Colour(6915853);
 
     @Test
-    void testIfConstructorValuesAreValidForRGBTypeFloatRed(){
+    void testIfParamTypeFloatConstructorLogicReturnsValidValueForRed(){
         assertEquals(230, colourFloat.getRed());
     }
 
+
     @Test
-    void testIfConstructorValuesAreValidForRGBTypeFloatGreen(){
+    void testIfParamTypeFloatConstructorLogicReturnsValidValueForGreen(){
         assertEquals(153, colourFloat.getGreen());
     }
 
     @Test
-    void testIfConstructorValuesAreValidForRGBTypeFloatBlue(){
+    void testIfParamTypeFloatConstructorLogicReturnsValidValueForBlue(){
         assertEquals(128, colourFloat.getBlue());
     }
 
     @Test
-    void exceptionTestingForTheConstructorThatTakesInRGBTypeFloat() {
+    void exceptionTestingForTheConstructorThatTakesInParamTypeFloat() {
         assertThrows(IllegalArgumentException.class,
                 () -> new Colour(20,40,20),
                 "Float values are not valid!"
@@ -31,22 +32,22 @@ class ColourTest {
     }
 
     @Test
-    void testIfConstructorValuesAreValidForRGBTypeIntRed(){
+    void testIfParamTypeIntConstructorLogicReturnsValidValueForRed(){
         assertEquals(105, colourInt.getRed());
     }
 
     @Test
-    void testIfConstructorValuesAreValidForRGBTypeIntGreen(){
+    void testIfParamTypeIntConstructorLogicReturnsValidValueForGreen(){
         assertEquals(135, colourInt.getGreen());
     }
 
     @Test
-    void testIfConstructorValuesAreValidForRGBTypeIntBlue(){
+    void testIfParamTypeIntConstructorLogicReturnsValidValueForBlue(){
         assertEquals(13, colourInt.getBlue());
     }
 
     @Test
-    void exceptionTestingForTheConstructorThatTakesInRGBTypeInt() {
+    void exceptionTestingForTheConstructorThatTakesInParamTypeInt() {
         assertThrows(IllegalArgumentException.class,
                 () -> new Colour(999999999),
                 "Integer value is not valid!"
@@ -54,37 +55,37 @@ class ColourTest {
     }
 
     @Test
-    void compareColoursBetweenAColourOfTypeIntAndAColourOfTypeFloatComparisonResultIsFalse() {
+    void compareColoursBetweenAColourOfTypeIntAndAColourOfTypeFloatComparisonResultShouldBeFalse() {
         assertFalse(Colour.compareColours(colourFloat, colourInt));
     }
 
     @Test
-    void compareColoursBetweenAColourOfTypeIntAndAColourOfTypeFloatComparisonResultIsTrue() {
+    void compareColoursBetweenAColourOfTypeIntAndAColourOfTypeFloatComparisonResultShouldBeTrue() {
         var c1 = new Colour(16777215);
         var c2 = new Colour(1.0f,1.0f,1.0f);
         assertTrue(Colour.compareColours(c1, c2));
     }
 
     @Test
-    void compareColoursBetweenTwoColoursOfTypeIntComparisonResultIsTrue() {
+    void compareColoursBetweenTwoColoursOfTypeIntComparisonResultShouldBeTrue() {
         var colour = new Colour(6915853);
         assertTrue(Colour.compareColours(colourInt, colour));
     }
 
     @Test
-    void compareColoursBetweenTwoColoursOfTypeIntComparisonResultIsFalse() {
+    void compareColoursBetweenTwoColoursOfTypeIntComparisonResultShouldBeFalse() {
         var colour = new Colour(16715);
         assertFalse(Colour.compareColours(colourInt, colour));
     }
 
     @Test
-    void compareColoursBetweenTwoColoursOfTypeFloatComparisonResultIsTrue() {
+    void compareColoursBetweenTwoColoursOfTypeFloatComparisonResultShouldBeTrue() {
         var colour = new Colour(0.9f,0.6f,0.5f);
         assertTrue(Colour.compareColours(colourFloat, colour));
     }
 
     @Test
-    void compareColoursBetweenTwoColoursOfTypeFloatComparisonResultIsFalse() {
+    void compareColoursBetweenTwoColoursOfTypeFloatComparisonResultShouldBeFalse() {
         var colour = new Colour(0.0f,0.1f,0.5f);
         assertFalse(Colour.compareColours(colourFloat, colour));
     }
